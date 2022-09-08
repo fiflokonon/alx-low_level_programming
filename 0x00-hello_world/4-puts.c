a6768b2325c7be9d75b47760efe540244635c7b5
fiflokonon <fiflokonon@gmail.com>
2022-09-08 05:28:12 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-int main (void) {
+/**
+ *Code for something print
+ * */
+int main(void) {
    puts("\"Programming is like building a multilingual puzzle");
    return(0);
 }
