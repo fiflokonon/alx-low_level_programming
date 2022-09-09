1b5348f63b4af62ef7c80038318b77092ceaf28f
fiflokonon <fiflokonon@gmail.com>
2022-09-09 04:41:35 -0700

    var

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succcess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(""+i);
+	putchar('\n');
+
+	return (0);
+}
