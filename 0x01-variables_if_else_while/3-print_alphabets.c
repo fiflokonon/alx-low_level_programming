9a69052ce8a8b434df626473beb57eaf24aeec37
fiflokonon <fiflokonon@gmail.com>
2022-09-09 03:10:14 -0700

    var

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for(alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
