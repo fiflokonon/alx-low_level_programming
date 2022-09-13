f9ddd2e518aa81ed71596ebe872f421ddbd10874
fiflokonon <fiflokonon@gmail.com>
2022-09-13 03:12:15 -0700

    loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,9 +14,7 @@ void print_alphabet_x10(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
-		{
-			_putchar(alpha);
-		}
+			_putchar(alpha);	
 		_putchar('\n');
 	}
 }
