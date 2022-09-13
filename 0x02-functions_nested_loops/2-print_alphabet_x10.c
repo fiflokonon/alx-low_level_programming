9325572b48baff49fdacc20c3096712c179cf23b
fiflokonon <fiflokonon@gmail.com>
2022-09-13 03:01:27 -0700

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Check desription
+ * Description: prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: Nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		print_alphabet();
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define HEADER_FILE
 
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
 #endif
