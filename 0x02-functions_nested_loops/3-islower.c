1e6fd8904c7b46cf3b154fc0f81bd064e35e4d68
fiflokonon <fiflokonon@gmail.com>
2022-09-13 05:19:36 -0700

    fn

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,14 @@
 
 /**
  * _islower - Check Description
- * Description: 
+ * Description:check for lowercase
  *
  * Return: 1 (Success) 0 (Failure)
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
