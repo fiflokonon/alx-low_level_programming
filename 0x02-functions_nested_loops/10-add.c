160b2bd58d1577c0503b77643760ff88996f4bb8
fiflokonon <fiflokonon@gmail.com>
2022-09-13 06:43:54 -0700

    fn

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Check Description
+ * @n1: An input character
+ * @n2: An input character
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int n1, int n2)
+{
+	int r;
+
+	r = n1 + n2;
+
+	return (r);
+}
