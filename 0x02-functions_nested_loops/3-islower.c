55293fecb3cf39579f030dc1b781c4b39e8de5fe
fiflokonon <fiflokonon@gmail.com>
2022-09-13 04:33:11 -0700

    loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,14 +8,8 @@
  */
 int _islower(int c)
 {
-	char i;
-	int lower = 0;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == c)
-			lower = 1;
-	}
-
-	return (lower);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
