51dd932890527cfe656a4ad4f440c063dbed8ca9
fiflokonon <fiflokonon@gmail.com>
2022-09-15 05:31:32 -0700

    more

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Check Description
+ * Description: Prints numbers from 0 to 9
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9;i++)
+		_putchar(i);
+	_putchar('\n');
+}
