a9aab41719af2cb80e739d11291a3499fc84fc60
fiflokonon <fiflokonon@gmail.com>
2022-09-15 05:12:17 -0700

    more

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Check Description
+ * Description: Return integers multiplication
+ * @a: An input
+ * @b: An input
+ * Return: Result of the multiplication
+ */
+int mult(int a, int b)
+{
+	return (a * b);
+}
