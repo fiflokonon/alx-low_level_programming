f74d838728d11c54620fccb93d4dbca686d17693
fiflokonon <fiflokonon@gmail.com>
2022-09-19 12:12:04 -0700

    point

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
