2de1350c070502173fe204a2e962d59143698677
fiflokonon <fiflokonon@gmail.com>
2022-09-19 12:17:47 -0700

    point

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _atoi - a function that converts string to integer
@@ -8,5 +7,28 @@
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+
+		else if (null_flag)
+			break;
+		s++;
+	}
+
+	if (sign < 0)
+		total = (-total);
+
+	return (total);
 }
