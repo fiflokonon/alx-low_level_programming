b70df35f54c19316a3fc46d01529da04ecfc8109
fiflokonon <fiflokonon@gmail.com>
2022-09-22 02:24:41 -0700

    mpoint

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - check Description
+ * Description:  That concat two string
+ * Return: Nothing
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
