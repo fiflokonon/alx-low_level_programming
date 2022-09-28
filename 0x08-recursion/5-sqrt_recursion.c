7921e276663e0c05ab50b02ef9a66b80e2b91d03
fiflokonon <fiflokonon@gmail.com>
2022-09-28 11:52:17 -0700

    recu

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - It returns the value of square root of n.
+ * @n: an input integer
+ * Return: The square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
