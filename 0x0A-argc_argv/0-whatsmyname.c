ff05ef54d20703623ccfce39c8b857348c3325b3
fiflokonon <fiflokonon@gmail.com>
2022-09-30 04:05:31 -0700

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main - check entry point
- * Description: Print name
- * Return: Always 0 Success
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[]) { 
