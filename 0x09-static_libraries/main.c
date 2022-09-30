fe3532241cd14cd1649adef606c430d4aaff8e81
fiflokonon <fiflokonon@gmail.com>
2022-09-30 13:02:02 -0700

    lib

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -check entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
