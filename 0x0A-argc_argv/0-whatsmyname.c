23bf742f2385b298c1e0aeafb66443f4e2eabee9
fiflokonon <fiflokonon@gmail.com>
2022-09-30 03:59:04 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check entry point
+ * Description: Print name
+ * Return: Always 0 Success
+ */
+
+int main() {
+    // Write C code here
+    printf("%s", argv[0]);
+    return (0);
+}
