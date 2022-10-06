10348d2840950810a481a40286e2d38a3536b6c3
fiflokonon <fiflokonon@gmail.com>
2022-10-06 10:22:02 -0700

    free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,5 +9,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (calloc(nmemb, size));
+	unsigned int i = 0;
+	char *space;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	for (; i < nmemb * size; i++)
+		*(space + i) = 0;
+
+	return (space);
 }
