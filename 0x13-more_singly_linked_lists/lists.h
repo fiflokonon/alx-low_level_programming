1b1b88de39044fc64df761b8cde362f12d6dddb8
fiflokonon <fiflokonon@gmail.com>
2022-10-25 07:51:19 -0700

    oh

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,7 +14,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,5 +29,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 void free_list(listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* _LISTS_H_*/
